da87fd7d3fda9945f097b55b4ede27ba5f1eef6e
Mary Webby <MaryWebby@Marys-Air-3.hsd1.il.comcast.net>
2023-09-12 15:19:14 -0500

    py work 9_12

---
+++ hello_world.py
@@ -0,0 +1,34 @@
+print("hello python world!")
+
+# printing messages to the terminal requires indicating of the python interpreter. for example "python3 hello_world.py". 
+message = "hello python pt.2!"
+print(message)
+
+#VARIABLES
+
+# variable naming should be descriptive and concise, varibales should be all lowercase (uppercase inciating other meanings later on in a different chapter)
+# varibables should also include underscores between words not dashes
+
+# traceback, term used when error handling, example provided 
+message = "hello traceback"
+# print(mesage)
+# when python3 hello_world.py is run in the terminal, the message showing that the traceback recived the most recent call in line 13 before it reached 'mesage'
+
+
+#STRINGS
+
+# a data type, series of characters such as "hello", anything inside quotes can be considered a string
+# can be used with single or double quotes
+# for example
+message = 'A wise man once said, "life is a highway"!'
+print(message)
+
+    # changing case in a string with methods 
+name = "tom jones"
+print(name.title())
+# similar actions 
+# print(name.upper())
+# print(name.lower())
+    
+    # using varibles in strings 
+
