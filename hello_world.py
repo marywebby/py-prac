1919412914b1ada9ad947dfec002bb31e99c993b
Mary Webby <MaryWebby@android-1de82f31911037b0.localdomain>
2023-09-13 12:31:31 -0500

    py work 9_13

--- hello_world.py
+++ hello_world.py
@@ -6,29 +6,67 @@ print(message)
 
 #VARIABLES
 
-# variable naming should be descriptive and concise, varibales should be all lowercase (uppercase inciating other meanings later on in a different chapter)
-# varibables should also include underscores between words not dashes
+    # variable naming should be descriptive and concise, varibales should be all lowercase (uppercase inciating other meanings later on in a different chapter)
+    # varibables should also include underscores between words not dashes
 
-# traceback, term used when error handling, example provided 
+    # traceback, term used when error handling, example provided 
 message = "hello traceback"
-# print(mesage)
-# when python3 hello_world.py is run in the terminal, the message showing that the traceback recived the most recent call in line 13 before it reached 'mesage'
+    # print(mesage)
+    # when python3 hello_world.py is run in the terminal, the message showing that the traceback recived the most recent call in line 13 before it reached 'mesage'
 
 
 #STRINGS
 
-# a data type, series of characters such as "hello", anything inside quotes can be considered a string
-# can be used with single or double quotes
-# for example
+    # a data type, series of characters such as "hello", anything inside quotes can be considered a string
+    # can be used with single or double quotes
+    # for example
 message = 'A wise man once said, "life is a highway"!'
 print(message)
 
     # changing case in a string with methods 
 name = "tom jones"
 print(name.title())
-# similar actions 
-# print(name.upper())
-# print(name.lower())
+    # similar actions 
+    # print(name.upper())
+    # print(name.lower())
     
     # using varibles in strings 
+first_name = "calico"
+last_name = "critters"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+    # to put variables into a string, place an f before 
+    # another example
+print(f"Hello, {full_name.title()}!")
+first_name = "Calico"
+last_name = "Critters"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()} friends!"
+print(message)
 
+    # adding whitespace to strings with tabs or newlines
+    # whitespace = any nonprinting characters (spaces,tabs,end-of-line symbols), also use whitespace to organize output so its easier for users to read 
+    
+    # adding a tab
+print("\tI'm tabbed")
+    # adding newline to a string
+print("Languages: \nPython\nC\nJavaScript")
+    # now adding tab and list togehter 
+print("Languages: \n\tPython\n\tC\n\tJavaScript")
+    # stripping whitespace at the back of a string
+fav_language1 = " python "
+print(fav_language1.rstrip())
+    # stripping whitespace at the front of a string
+fav_language2 = " python "
+print(fav_language2.lstrip())
+    # stripping all of the white space 
+fav_language3 = " python "
+print(fav_language3.strip())
+    # removing prefixes, ex. a URL https://
+nostarch_url = 'https://nostarch.com'
+print(nostarch_url.removeprefix('https://'))
+    # 
+
+
+
+    
