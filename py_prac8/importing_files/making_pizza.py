89458d78f171a3a6816e2fa52b2feeaf101f5900
Mary Webby <MaryWebby@Marys-MacBook-Air-3.local>
2023-10-11 15:41:28 -0500

    chap 8 donegit add -A

---
+++ py_prac8/importing_files/making_pizza.py
@@ -0,0 +1,9 @@
+import pizza
+# also
+# from pizza import make_pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# also
+# make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# make_pizza(16, 'pepperoni')
